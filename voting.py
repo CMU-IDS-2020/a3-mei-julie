368190172ce75fe8a214c22bf3518e7ef88f2665
Mei May Leng <mayleng@Meis-MacBook-Pro-2.local>
2020-10-16 15:02:06 -0700

    updated years

--- voting.py
+++ voting.py
@@ -14,8 +14,8 @@ def query(query):
     df=con.fetchdf()
     return df
 
-def make_map(title,w,h,source):
-    return alt.Chart(states,title=title).mark_geoshape().encode(
+def make_map(w,h,source):
+    return alt.Chart(states).mark_geoshape().encode(
         color='p_votes:Q'
     ).transform_lookup(
         lookup='id',
@@ -37,7 +37,7 @@ con=create_db(create_db_comm)
 comm="""create table p_votes as select state,year,party,cast(candidatevotes as float)/cast(totalvotes as float) as perc_votes from votes"""
 df=query(comm)
 command ="""create table extra as (select m.state,m.year,m.party,t.votes
-        from (
+            from (
             select state,year,max(perc_votes) as votes
             from p_votes
             group by state,year
@@ -48,28 +48,31 @@ df=query(command)
 create_db_comm="create table all_years as select * from read_csv_auto('xs.csv');"
 con=create_db(create_db_comm)
 
-comm="""create table year_2008 as select * from all_years
-        where year=2008"""
+comm="""create table year_1992 as select * from all_years
+        where year=1992"""
 df=query(comm)
-elec_year=st.slider("Year",min_value=1976,max_value=2016,step=4)
-# con.execute("COPY year_2008 TO 'y_2008.csv'")
+elec_year=st.slider("Year",min_value=2000,max_value=2016,step=4)
+# con.execute("COPY year_1992 TO 'y_1992.csv'")
 states = alt.topo_feature(data.us_10m.url, 'states')
+
 source00="https://raw.githubusercontent.com/CMU-IDS-2020/a3-mei-julie/master/y_2000.csv"
+source04="https://raw.githubusercontent.com/CMU-IDS-2020/a3-mei-julie/master/y_2004.csv"
 source08="https://raw.githubusercontent.com/CMU-IDS-2020/a3-mei-julie/master/y_2008.csv"
 source12 = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-mei-julie/master/y_2012.csv"
 source16="https://raw.githubusercontent.com/CMU-IDS-2020/a3-mei-julie/master/y_2016.csv"
-# source="https://raw.githubusercontent.com/CMU-IDS-2020/a3-mei-julie/master/map_info.csv"
 
 us=None
 w=800
 h=500
 if elec_year==2000:
-    us=make_map("title",w,h,source00)
+    us=make_map(w,h,source00)
+elif elec_year==2004:
+    us=make_map(w,h,source04)
 elif elec_year==2008:
-    us=make_map("title",w,h,source08)
+    us=make_map(w,h,source08)
 elif elec_year==2012:
-    us=make_map("title",w,h,source12)
+    us=make_map(w,h,source12)
 elif elec_year==2016:
-    us=make_map("title",w,h,source16)
+    us=make_map(w,h,source16)
 st.write(us)
 
