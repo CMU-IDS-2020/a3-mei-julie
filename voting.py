fbfafb16c967cc579f018d524e7094b17febca36
Mei May Leng <mayleng@Meis-MBP-2.attlocal.net>
2020-10-20 20:32:18 -0700

    MERGEEDDDD

--- voting.py
+++ voting.py
@@ -84,12 +84,6 @@ source2 = c.loc[c["Year"] == 2004]
 source3 = c.loc[c["Year"] == 2008]
 source4 = c.loc[c["Year"] == 2012]
 source5 = c.loc[c["Year"] == 2016]
-#st.write(c)
-#st.write(source1)
-#st.write(source2)
-#st.write(source3)
-#st.write(source4)
-#st.write(source5)
 
 year2000=make_chart(source1)
 
@@ -100,12 +94,6 @@ year2008=make_chart(source3)
 year2012=make_chart(source4)
 
 year2016=make_chart(source5)
-
-# st.write(year2000)
-# st.write(year2004)
-# st.write(year2008)
-# st.write(year2012)
-# st.write(year2016)
 
 ####INCOME#####
 allyear_income=query("select * from income_t where year !=2000")
@@ -140,7 +128,50 @@ dem_type = st.selectbox(
     "Select a category:",
     ("Age", "Income", "Race")
 )
+#####RACE#####
+url="https://raw.githubusercontent.com/CMU-IDS-2020/a3-mei-julie/master/racedata.csv"
+s=requests.get(url).content
+c=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
+source1 = c.loc[c["Race"] == "Asian or Pacific Islander"]
+source2 = c.loc[c["Race"] == "Black"]
+source3 = c.loc[c["Race"] == "Hispanic"]
+source4 = c.loc[c["Race"] == "White"]
+
+
+asianchart=alt.Chart(source1).mark_area(opacity=0.5).encode(
+    x="Year:O",
+    y=alt.Y("Number_of_Votes:Q", stack=None, title='[Asian] Number of Votes'),
+    color=alt.Color("Label:N",
+            scale=alt.Scale(
+                range=['#ffcc5c','#96ceb4','steelblue']))
+).properties(height=300, width=500)
+
+blackchart=alt.Chart(source2).mark_area(opacity=0.5).encode(
+    x="Year:O",
+    y=alt.Y("Number_of_Votes:Q", stack=None, title='[Black] Number of Votes'),
+    color=alt.Color("Label:N",
+            scale=alt.Scale(
+                range=['#ffcc5c','#96ceb4','steelblue']))
+).properties(height=300, width=500)
+
+hispanicchart=alt.Chart(source3).mark_area(opacity=0.5).encode(
+    x="Year:O",
+    y=alt.Y("Number_of_Votes:Q", stack=None, title='[Hispanic] Number of Votes'),
+    color=alt.Color("Label:N",
+            scale=alt.Scale(
+                range=['#ffcc5c','#96ceb4','steelblue']))
+).properties(height=300, width=500)
+
+whitechart=alt.Chart(source4).mark_area(opacity=0.5).encode(
+    x="Year:O",
+    y=alt.Y("Number_of_Votes:Q", stack=None, title='[White] Number of Votes'),
+    color=alt.Color("Label:N",
+            scale=alt.Scale(
+                range=['#ffcc5c','#96ceb4','steelblue']))
+).properties(height=300, width=500)
+
+####WRITE GRAPHS######
 if dem_type=='Age':
     if elec_year==2000:
         st.write(year2000)
@@ -193,7 +224,10 @@ elif dem_type=='Income':
         )
     st.write(income)
 elif dem_type=='Race':
-    st.write('race')
+    st.write(whitechart)
+    st.write(blackchart)
+    st.write(asianchart)
+    st.write(hispanicchart)
 
 
 ###POLITICAL VIS####
